5af15a0627d6588cfa8a0cd8f3c4edfb3c5f1d50
Carlos Eduardo Raez Escarcena <carlos.raez@grupomok.com>
2019-07-02 18:27:29 -0500

    c2

---
+++ Sales.Backend/Models/LocalDataContext.cs
@@ -0,0 +1,15 @@
+﻿namespace Sales.Backend.Models
+{
+    using Domain.Models;
+
+    #region Explicacion
+    /*  
+     *  Esto se hace para facilitar el proeso de migracion de la base de datos.
+     *  Se hereda a la clase DataContext, la cual inicia la base de datos.
+    */
+    #endregion
+    public class LocalDataContext:DataContext
+    {
+        public System.Data.Entity.DbSet<Sales.Common.Models.Product> Products { get; set; }
+    }
+}
---
+++ Sales.Domain/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿namespace Sales.Domain.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext:DbContext
+    {
+        #region Explicacion
+        /*
+         * Al heredar la clase "base" y enviar el nombre de la cadena de conexion el 
+         * contructor de la clase DataContext inicia la coneccion a la base de datos.
+        */    
+        #endregion
+        public DataContext():base("Sales"){}
+    }
+}
